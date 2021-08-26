c7d7e041ff064b536bdba94a377e29092eb2000c
kawadumax <kazmax7@gmail.com>
2021-08-26 17:49:48 +0900

    不要な型宣言を削除

--- src/shims-vuex.d.ts
+++ src/shims-vuex.d.ts
@@ -1,12 +1,7 @@
 import { ComponentCustomProperties } from "vue";
 import { Store } from "vuex";
-
+import { State } from "@/store";
 declare module "@vue/runtime-core" {
-  // declare your own store states
-  interface State {
-    currentNoteName: string;
-  }
-
   // provide typings for `this.$store`
   interface ComponentCustomProperties {
     $store: Store<State>;
