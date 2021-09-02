93d861c184e5a7b2f4802f75527a4e87871a7424
kawadumax <kazmax7@gmail.com>
2021-09-02 17:56:28 +0900

    Elements導入

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
+  <ElHeader>
+    <h1>FUYOMI</h1>
+  </ElHeader>
   <Score></Score>
   <Answer></Answer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Answer from './components/Answers.vue'
+import Answer from '@/components/Answers.vue'
 import Score from '@/components/Score.vue'
+import { ElHeader } from 'element-plus'
 export default defineComponent({
   name: 'App',
   components: {
     Answer,
-    Score
+    Score,
+    ElHeader
   }
 });
 </script>
@@ -21,7 +26,7 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  margin-top: 60px;
+  /* margin-top: 60px; */
   display: flex;
   justify-content: center;
   flex-flow: column;
