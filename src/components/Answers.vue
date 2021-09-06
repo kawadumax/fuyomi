eda1f30c076da0d2242c59d33d9ca48999e643aa
kawadumax <kazmax7@gmail.com>
2021-09-06 16:04:28 +0900

    クリックイベントを発行できない[wip]

--- src/components/Answers.vue
+++ src/components/Answers.vue
@@ -1,9 +1,9 @@
 <template>
-    <section>
+    <section id="answer-section">
         <ul id="answer">
-            <li v-for="note in scale" :key="note">
+            <!-- <li v-for="note in scale" :key="note">
                 <button :value="note" v-on:click="submit($event)">{{ note }}</button>
-            </li>
+            </li>-->
         </ul>
         <Result :result="currentAnswer" :is-anim="isAnim" @after-animated="onAfterAnimated"></Result>
     </section>
@@ -15,6 +15,8 @@ import { useStore } from '@/store'
 import Result from "./Result.vue";
 import Constant from "@/lib/types"
 import { defineComponent } from 'vue';
+import { SVG } from '@svgdotjs/svg.js'
+
 export default defineComponent({
     setup() {
         // Storeを取得する
@@ -26,6 +28,9 @@ export default defineComponent({
             currentAnswer: Constant.Result.None,
             isAnim: false
         }
+    },
+    mounted() {
+        this.drawKey();
     },
     components: {
         Result
@@ -47,6 +52,27 @@ export default defineComponent({
             this.$store.commit('changeNote');
 
         },
+        drawKey() {
+
+            let draw = SVG().addTo('#answer').size(this.scale.length * 20, 100)
+            this.scale.forEach((noteName, i) => {
+                let group = draw.group();
+                let rect = group.rect(20, 100).attr({ fill: 'none', stroke: '#222' })
+                var text_name = group.text(noteName[0])
+                    .font({ size: 20, family: 'Helvetica' })
+                    .center(0.5 * (rect.width() as number), 0.75 * (rect.height() as number));
+                var text_oct = group.text(noteName[1])
+                    .font({ size: 20, family: 'Helvetica' })
+                    .center(0.5 * (rect.width() as number), 0.9 * (rect.height() as number));
+                // rect.click(this.submit);
+                // group.click(this.submit);
+                group.mouseover(function (this: any) {
+                    console.log(this);
+                    this.addr({ fill: 'red' });
+                })
+                group.move(20 * i, 0);
+            })
+        }
     }
 })
 </script>
@@ -60,5 +86,6 @@ li {
 ul {
     display: flex;
     justify-content: center;
+    padding: 0px;
 }
 </style>
